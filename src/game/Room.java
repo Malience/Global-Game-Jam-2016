380baac159330b46b6555095ac75702196366725
Chase Schelthoff <schelthoff.chase@gmail.com>
2016-01-29 21:10:27 -0500

    Changed Room implementation added Map

--- src/game/Room.java
+++ src/game/Room.java
@@ -2,15 +2,16 @@ package game;
 
 import com.base.engine.core.math.Vector3f;
 
+
 public class Room 
 {
-	private Vector3f x, y, z;
+	public static Vector3f roomSize = new Vector3f(10,10,10); //TODO: check room size
 	
-	public Room(Vector3f x, Vector3f y, Vector3f z)
+	private Vector3f position;
+	
+	public Room(Vector3f position)
 	{
-		this.x = x;
-		this.y = y;
-		this.z = z;
+		this.position = position;		
 	}
 	
 	

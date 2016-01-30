8bf6e397a82cc5a2641ad3d28040981c1d80214b
Chase Schelthoff <schelthoff.chase@gmail.com>
2016-01-29 20:16:07 -0500

    Added Room Class

---
+++ src/game/Room.java
@@ -0,0 +1,17 @@
+package game;
+
+import com.base.engine.core.math.Vector3f;
+
+public class Room 
+{
+	private Vector3f x, y, z;
+	
+	public Room(Vector3f x, Vector3f y, Vector3f z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+}

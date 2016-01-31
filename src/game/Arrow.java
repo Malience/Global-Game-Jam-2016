5539ad409e52e2224ab5dc9ac63fda3d31e96eef
Haru <jaclynwoodlief@gmail.com>
2016-01-31 05:32:10 -0500

    v

--- src/game/Arrow.java
+++ src/game/Arrow.java
@@ -1,21 +1,10 @@
 package game;
 
-import com.base.engine.components.MeshRenderer;
-import com.base.engine.rendering.Material;
-import com.base.engine.rendering.Mesh;
-import com.base.engine.rendering.Texture;
-
 public class Arrow extends Gear 
 {
 	public Arrow (float x, float y, float z)
 	{
 		super (x,y,z);
-		
-		Material m = new Material();
-		m.addTexture("Arrow", new Texture("Arrow001.png"));
-		
-		Mesh n = new Mesh("Arrow001.obj");
-		MeshRenderer mR = new MeshRenderer(n, m);
 	}
 	
 	public String toString()

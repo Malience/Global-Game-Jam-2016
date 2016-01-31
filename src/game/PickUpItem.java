a3232210c3c717c58750a18995802ae6aa021732
Haru <jaclynwoodlief@gmail.com>
2016-01-31 02:43:38 -0500

    ugh

--- src/game/PickUpItem.java
+++ src/game/PickUpItem.java
@@ -3,6 +3,8 @@ package game;
 import com.base.engine.components.GameComponent;
 import com.base.engine.core.GameObject;
 import com.base.engine.core.Input;
+import com.base.engine.rendering.RenderingEngine;
+import com.base.engine.rendering.UI.UIText;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_E;
 
@@ -12,9 +14,11 @@ public class PickUpItem extends GameComponent
 	private int grabKey = GLFW_KEY_E;
 	private boolean grabable = true;
 	
+	UIText message = new UIText(300,300,"timesNewRoman.png",  " ", 24);
+	
 	public PickUpItem()
 	{
-		
+		RenderingEngine.UI.add(message);
 	}
 	
 	public void collided(GameObject go)
@@ -24,8 +28,15 @@ public class PickUpItem extends GameComponent
 			//grabable = true;
 			//if(object is grabable)
 			//{
+			//	message.text = "Pick-Up (e)";
+			// 	message.generate();
 			//	pickUp(go);
 			//}
+		//else
+		//{
+		//		message.text = " ";
+		//		message.generate();
+		//}
 	}
 	
 	public void pickUp(GameObject go)

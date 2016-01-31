de71ad358d9bb0201b1a156d3203d2cdbd9efba4
Chase Schelthoff <schelthoff.chase@gmail.com>
2016-01-30 21:30:08 -0500

    Added correct indices to form a cube

--- src/game/Room.java
+++ src/game/Room.java
@@ -136,25 +136,25 @@ public class Room extends GameObject
 		vertices[7] = new Vertex(new Vector3f(x - halfX, y - halfY, z - halfZ));
 		
 		
-		int bi[] = { 0, 1, 2,
-				2, 1, 3,
+		//DO NOT CHANGE THESE UNLESS YOU KNOW WHAT YOU'RE DOING
+		int bi[] = {
+				7, 5, 4,
+				4, 6, 7,
 				
-				//Bottom
-				1, 3, 5,
-				5, 1, 7,
+				3, 2, 0,
+				0, 1, 3,
 				
-				//Top
-				0, 2, 4,
-				4, 2, 6,
+				4, 0, 2,
+				2, 6, 4,
 				
-				2, 3, 4,
-				4, 3, 5,
+				7, 3, 1,
+				1, 5, 7,
 				
-				4, 5, 6,
-				6, 5, 7,
+				5, 1, 0,
+				0, 4, 5,
 				
-				6, 7, 0,
-				0, 7, 1
+				6, 2, 3,
+				3, 7, 6,
 		};
 		mesh = new Mesh(vertices, bi);
 		

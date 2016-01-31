a1be53f287253b74c4d499574b5178115b5e7d93
Haru <jaclynwoodlief@gmail.com>
2016-01-31 04:07:28 -0500

    clean and added junk

--- src/game/SpikeTrap.java
+++ src/game/SpikeTrap.java
@@ -1,22 +1,10 @@
 package game;
 
-import com.base.engine.components.MeshRenderer;
-import com.base.engine.rendering.Material;
-import com.base.engine.rendering.Mesh;
-import com.base.engine.rendering.Texture;
-
 public class SpikeTrap extends Gear
 {
 	public SpikeTrap (float x, float y, float z)
 	{
 		super (x,y,z);
-		
-		Material m = new Material();
-		m.addTexture("Spike", new Texture("Spike001.png"));
-		
-		Mesh n = new Mesh("Spike001.obj");
-		MeshRenderer mR = new MeshRenderer(n, m);
-
 	}
 	
 	public String toString()
--- src/game/Tripwire.java
+++ src/game/Tripwire.java
@@ -1,19 +1,9 @@
 package game;
 
-import com.base.engine.rendering.Material;
-import com.base.engine.rendering.Mesh;
-import com.base.engine.rendering.Texture;
-
 public class Tripwire extends Gear
 {
 	public Tripwire (float x, float y, float z)
 	{
 		super (x,y,z);
-		
-		Material m = new Material();
-		m.addTexture("Testing", new Texture("brown.png"));
-		
-		Mesh n = new Mesh("");
-
 	}
 }
